a2f93162a76361835bea288b94947b015edc2703
William Daggett <william.daggett@ndsu.edu>
2026-05-09 18:07:19 -0500

    Initial commit

---
+++ src/gp4movierentalapp/userInterface.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gp4movierentalapp;
+
+/**
+ *
+ * @author Will Daggett
+ */
+public class userInterface {
+
+    /**
+     * @param args the command line arguments
+     * Make sure to change port and extension on jdbcURL to match local conditions
+     */
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:postgresql://localhost:5433/Gp4MovieRentalApp";
+        String username = "postgres";
+        String password = "0";
+    }
+    
+}
